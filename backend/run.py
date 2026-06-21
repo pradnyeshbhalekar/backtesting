ba9dd092a692d618829a8f814d422180b0d550af
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-06-21 14:01:16 +0530

    deployment fixes

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    debug = os.environ.get("FLASK_ENV") == "development"
+    app.run(host="0.0.0.0", port=port, debug=debug)
